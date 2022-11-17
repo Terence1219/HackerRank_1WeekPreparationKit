f5dd3921a4f657e90fef0664dd5f12ecdd8d2546
Terence1219 <terence1219@gmail.com>
2022-11-17 23:42:10 +0800

    add fliping the matrix

---
+++ day2/FlippingtheMatrix.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(matrix):
+    # Write your code here
+    m = len(matrix)
+    n = m//2
+    maxsum = 0
+    
+    for i in range(n):
+        for j in range(n):
+            maxsum  += max(matrix[i][j],matrix[i][m-j-1],matrix[m-i-1][j],matrix[m-i-1][m-j-1])
+    return maxsum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        matrix = []
+
+        for _ in range(2 * n):
+            matrix.append(list(map(int, input().rstrip().split())))
+
+        result = flippingMatrix(matrix)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
